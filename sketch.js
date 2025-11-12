a3a7cf20265622bb0b74e2f109b6bd2ea762afaa
codemasterfunk <148785150+codemasterfunk@users.noreply.github.com>
2025-11-12 08:39:33 -0500

    Implemente paused functionality

--- sketch.js
+++ sketch.js
@@ -12,28 +12,29 @@ function setup() {
 }
 
 function draw() {
+  if (paused) {
+    // We're paused, we can draw PAUSED stuff on the screen
+  } else {
+    background(450);
 
-  // UPDATE CODE HERE: What do we want to do here? We should check if we're paused and, if so, skip all the other drawing code.
-
-  background(450);
-
-  loadPixels();
-  for (x = 0; x < width; x++) {
-    for (y = 0; y < height; y++) {
-      let sum = 0;
-      for (i = 0; i < blobs.length; i++) {
-        let xdif = x - blobs[i].x;
-        let ydif = y - blobs[i].y;
-        let d = sqrt((xdif * xdif) + (ydif * ydif));
-        sum += 20 * blobs[i].r / d;
+    loadPixels();
+    for (x = 0; x < width; x++) {
+      for (y = 0; y < height; y++) {
+        let sum = 0;
+        for (i = 0; i < blobs.length; i++) {
+          let xdif = x - blobs[i].x;
+          let ydif = y - blobs[i].y;
+          let d = sqrt((xdif * xdif) + (ydif * ydif));
+          sum += 20 * blobs[i].r / d;
+        }
+        set(x, y, color(sum, 255, 255));
       }
-      set(x, y, color(sum, 255, 255));
     }
-  }
-  updatePixels();
+    updatePixels();
 
-  for (i = 0; i < blobs.length; i++) {
-    blobs[i].update();
+    for (i = 0; i < blobs.length; i++) {
+      blobs[i].update();
+    }
   }
 }
 
@@ -41,10 +42,8 @@ function keyPressed() {
   if (key === 'p') {
     if (paused === true) {
       paused = false;
-      loop();
     } else {
       paused = true;
-      noLoop();
     }
   }
 }
