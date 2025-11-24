7fe33b8f9721e187e00345e3b2fce540125298e0
calebruble16 <caleb.ruble16@gmail.com>
2025-11-24 12:35:56 -0500

    changed back to 500 cause it got laggie

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let paused = false;
 var blobs = []
 
 function setup() {
-  createCanvas(1366, 768);
+  createCanvas(500, 500);
   colorMode(HSB);
   for (i = 0; i < 20; i++) blobs.push(new Blob(random(0, width), random(0, height)));
 }
